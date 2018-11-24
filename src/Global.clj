a76a31bf2168f99bab1e2559a949bdcbc127d2f4
Islon Scherer <islon.scherer@rewe-digital.com>
2018-11-24 14:23:27 +0100

    fixed bug in foreign function

--- src/Global.clj
+++ src/Global.clj
@@ -1,8 +1,9 @@
-(ns Global._foreign)
+(ns Global._foreign
+  (:require [clojure.string :as s]))
 
 (def nan Double/NaN)
 
-(def infinity  Double/POSITIVE_INFINITY)
+(def infinity Double/POSITIVE_INFINITY)
 
 (defn isNaN [^double n]
   (Double/isNaN n))
@@ -12,7 +13,9 @@
 
 (defn readInt [^long radix]
   (fn [^String n]
-    (Integer/parseInt n radix)))
+    (-> (s/replace n #"(?i:0x|\..*)" "")
+        (Long/parseLong radix)
+        (double))))
 
 (defn readFloat [^String f]
   (Double/parseDouble f))
